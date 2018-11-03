f7141fdb37de50f19ee21599f7ffe98a41cfd89d
Irvin Naylor <Inaylor003@gmail.com>
2018-11-03 00:19:59 -0400

    Player can auto-move and jump

---
+++ 49er_GameJam_F2018/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+Author: Irvin Naylor
+Last Change: Implemented auto movement and jumping
+Script summary
+    - Script starts by using Player's Rigidbody component to continuously move forward
+    - If the jump key is held down, the player jumps.
+list of fields
+    - moveSpeed: movement speed forward
+    - jumpForce: Upward jump force (how high can they jump?)
+(Any extra notes)
+*/
+
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public float jumpForce;
+
+    private Rigidbody2D RigidBody_A;
+
+	// Use this for initialization
+	void Start () {
+        RigidBody_A = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RigidBody_A.velocity = new Vector2(moveSpeed, RigidBody_A.velocity.y);
+
+        //is the character jumping?
+
+        if (Input.GetKeyDown(KeyCode.W) )
+        {
+            RigidBody_A.velocity = new Vector2(RigidBody_A.velocity.x, jumpForce);
+        }
+	}
+}
