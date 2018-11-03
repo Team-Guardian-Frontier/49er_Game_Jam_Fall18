c5afa85db5d7a45b6c2696547223b08bc243e780
Justin Carrasquillo <justin.carrasquillo2000@gmail.com>
2018-11-03 17:00:04 -0400

    Added the Speedometer prefab

---
+++ 49er_GameJam_F2018/Assets/Scripts/Speedometer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Speedometer : MonoBehaviour {
+    public float speed;
+    public Text speedTxt;
+    private Rigidbody2D playerSpeed;
+	// Use this for initialization
+	void Start () {
+
+        speed = 0;
+        speedTxt.text = " Mph";
+        playerSpeed = GameObject.Find("Player").GetComponent<Rigidbody2D>();
+
+	}
+	
+    
+
+	// Update is called once per frame
+	void Update () {
+
+        speedTxt.text = playerSpeed.velocity.x + "Mph";
+
+	}
+}
