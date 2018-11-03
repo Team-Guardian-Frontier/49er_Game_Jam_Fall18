402f7ccab02b01edf42e67d5f34a59cdcea510ba
Jacob Miller <jmill299@uncc.edu>
2018-11-03 12:03:17 -0400

    I frames and Timer

---
+++ 49er_GameJam_F2018/Assets/Scripts/Timer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+    public float time;
+    public Text timeText;
+	// Use this for initialization
+	void Start () {
+        time = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+        timeText.text = time.ToString();
+
+	}
+}
